86e66fb09f1dc906781f396e386865b828d18228
Chris McGowan <136050462+ChrisMcGowanAu@users.noreply.github.com>
2024-09-11 07:25:45 +1000

    Update log.h
    Modified the original:
     1) compile under C++
     2) Add 'note' log type
     3) reduced full path name to just the filename
     Chris McGowan 2024

--- src/log.h
+++ src/log.h
@@ -3,6 +3,11 @@
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the MIT license. See `log.c` for details.
+ * Modified the original rxi version to 
+ * 1) compile under C++
+ * 2) Add 'note' log type
+ * 3) reduced full path name to just the filename
+ * Chris McGowan 2024
  */
 
 #ifndef LOG_H
@@ -13,7 +18,7 @@
 #include <stdbool.h>
 #include <time.h>
 
-#define LOG_VERSION "0.1.0"
+#define LOG_VERSION "0.1.1"
 
 typedef struct {
   va_list ap;
@@ -28,15 +33,20 @@ typedef struct {
 typedef void (*log_LogFn)(log_Event *ev);
 typedef void (*log_LockFn)(bool lock, void *udata);
 
-enum { LOG_TRACE, LOG_DEBUG, LOG_INFO, LOG_WARN, LOG_ERROR, LOG_FATAL };
+// Default log level os LOG_WARN
+enum { LOG_TRACE=0, LOG_DEBUG=1, LOG_INFO=2, LOG_NOTE=3, LOG_WARN=4, LOG_ERROR=5, LOG_FATAL=6 };
 
 #define log_trace(...) log_log(LOG_TRACE, __FILE__, __LINE__, __VA_ARGS__)
 #define log_debug(...) log_log(LOG_DEBUG, __FILE__, __LINE__, __VA_ARGS__)
 #define log_info(...)  log_log(LOG_INFO,  __FILE__, __LINE__, __VA_ARGS__)
+#define log_note(...)  log_log(LOG_NOTE,  __FILE__, __LINE__, __VA_ARGS__)
 #define log_warn(...)  log_log(LOG_WARN,  __FILE__, __LINE__, __VA_ARGS__)
 #define log_error(...) log_log(LOG_ERROR, __FILE__, __LINE__, __VA_ARGS__)
 #define log_fatal(...) log_log(LOG_FATAL, __FILE__, __LINE__, __VA_ARGS__)
 
+#ifdef __cplusplus
+extern "C"{
+#endif
 const char* log_level_string(int level);
 void log_set_lock(log_LockFn fn, void *udata);
 void log_set_level(int level);
@@ -45,5 +55,9 @@ int log_add_callback(log_LogFn fn, void *udata, int level);
 int log_add_fp(FILE *fp, int level);
 
 void log_log(int level, const char *file, int line, const char *fmt, ...);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
